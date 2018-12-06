58b2e12e461f27964cb707a93cf6407b5c0d43cd
Yagora <tanguy.charon47@gmail.com>
2018-12-06 15:26:37 +0100

    Add powerline font

--- install.sh
+++ install.sh
@@ -10,6 +10,13 @@ git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/sickill/vim-monokai.git && mkdir ~/.vim/colors && cp vim-monokai/colors/monokai.vim ~/.vim/colors
 
-mkdir ~/.fonts
-cp awesome-terminal-fonts/build ~/.fonts
-fc-cache -fv ~/.fonts 
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo cp awesome-terminal-fonts/build /usr/share/fonts/
+sudo fc-cache -fv ~/.fonts
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+
+
+chsh -s /bin/zsh
